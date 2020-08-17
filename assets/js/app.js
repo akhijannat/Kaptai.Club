7499890820e6c410c9555c00465f84b12b882678
akhijannat <56310132+akhijannat@users.noreply.github.com>
2020-08-17 13:44:38 +0600

    js

--- assets/js/app.js
+++
@@ -1,9 +0,0 @@
-$().ready(function(){
-    $('.slick-carousel').slick({
-      arrows: true,
-      centerPadding: "0px",
-      dots: true,
-      slidesToShow: 1,
-      infinite: false
-    });
-  });
